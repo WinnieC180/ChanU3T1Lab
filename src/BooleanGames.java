47b5f53e2391750ae2e40abfbb25c73fce3b76ef
1E10 Laptop <sample@bths.edu>
2022-10-24 12:27:24 -0400

    done with lab

---
+++ src/BooleanGames.java
@@ -0,0 +1,72 @@
+public class BooleanGames {
+    private int number;  // instance variable
+
+    // constructor
+    public BooleanGames(int number) {
+        this.number = number;
+    }
+
+    /* Returns true if the instance variable, number, is negative;
+       returns false otherwise */
+    public boolean isNegative() {
+        return (number < 0);
+    }
+
+    /* Returns true if the instance variable, number, is greater than num */
+    public boolean exceeds(int num){
+        if (number > num) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /* Returns true if the instance variable, number, is even;
+       returns false otherwise */
+    public boolean isEven() {
+        if (Math.abs(number) % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /* Returns true if the instance variable, number, is odd; returns false
+       otherwise; needs to be able to handle case when number is negative */
+    public boolean isOdd() {
+        if (Math.abs(number) % 2 == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /* Returns true if the instance variable, number, is a multiple of x;
+       returns false otherwise */
+    public boolean isMultipleOf(int x) {
+        if (number%x == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /* Returns true if the ones digit of the instance variable, number, is equal
+       to num; e.g. if number is 437, then isOnesDigit(7) would return true and
+       isOnesDigit(5) would return false
+       This method should handle the case when number is negative, so if
+       number is -123, then isOnesDigit(3) should return true.
+
+       PRECONDITION:  num >= 0
+     */
+    public boolean isOnesDigit(int num) {
+        int onesDigit = ((Math.abs(number)%100)%10);
+
+        if (onesDigit == num) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
---
+++ src/main.java
@@ -0,0 +1,25 @@
+public class main {
+    public static void main(String[] arg) {
+        double e = 30.0;
+        double f = 30.0;
+        System.out.println(e == f);
+
+        double g = 0.5;
+        double h = (double) 1 / 2;
+        System.out.println(g == h);
+
+        double j = 1.0 / 3;
+        double k = 2.0 / 6;
+        System.out.println(j == k);
+
+        double m = 6.66666666666;
+        double n = 6.66666666667;
+        System.out.println(m == n);
+
+        double p = 1.0 / 3;
+        double q = 1 - 2.0 / 3;
+        System.out.println(p == q);
+        System.out.println(p);
+        System.out.println(q);
+    }
+}
